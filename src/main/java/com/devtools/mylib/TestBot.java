c2dad02278243008eb466aad159da327fe9937ce
KlosZ <egorina2002@gmail.com>
2021-10-22 16:32:44 +0500

    first commit

---
+++ src/main/java/com/devtools/mylib/TestBot.java
@@ -0,0 +1,44 @@
+package com.devtools.mylib;
+
+import lombok.SneakyThrows;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class TestBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "@KKsCinemaBot";
+    }
+
+    @Override
+    public String getBotToken(){
+        return System.getenv("ACCESS_BOT_TOKEN");
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        TestBot bot = new TestBot();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
+    }
+
+    @Override
+    @SneakyThrows
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()){
+            Message message = update.getMessage();
+            if (message.hasText()){
+                String text = message.getText();
+                execute(SendMessage.builder()
+                        .chatId(message.getChatId().toString())
+                        .text("You sent: \n" + text)
+                        .build());
+            }
+        }
+    }
+}
