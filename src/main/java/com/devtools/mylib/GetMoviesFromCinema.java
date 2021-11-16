c0530ae804459648fce2aefdc638d1a1a656da77
kretatusha <92570098+kretatusha@users.noreply.github.com>
2021-11-16 10:44:46 +0500

    improve GetMoviesFromCinema

--- src/main/java/com/devtools/mylib/GetMoviesFromCinema.java
+++ src/main/java/com/devtools/mylib/GetMoviesFromCinema.java
@@ -1,74 +1,95 @@
 package com.devtools.mylib;
-import lombok.SneakyThrows;
-
 import java.net.*;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.*;
 
 
 public class GetMoviesFromCinema {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String engRus = findMovies("/ekaterinburg/cinema/titanik-sinema-361");
         System.out.println(engRus);
     }
 
-    public static Matcher getMatcher(String regex, String line) {
+    public static Matcher getMatcher(String regex, String line){
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(line);
     }
-
-    @SneakyThrows
-    public static String findMovies(String cinema) {
-        URL oracle = new URL("https://kassa.rambler.ru" + cinema);
-        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
-        StringBuilder result = new StringBuilder();
+    public static String findMovies(String cinema) throws Exception {
+        URL oracle = new URL("https://kassa.rambler.ru"+cinema);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+        Map<String,String> dictionary = new HashMap<String,String>();
+        String result ="";
         String regexMovie = "\s<div class=\"rasp_title\"><a href=\"[/a-z0-9]+\" itemprop=\"url\"><span itemprop=\"name\" class=\"s-name\">([\sa-zA-zа-яА-я0-9.:,]+)</span></a></div>";
         String regexGenre = "\s<div class=\"rasp_place\" itemprop=\"genre\">([а-яА-я,\s]+)</div>";
         String regexAge = "\s<div class=\"rasp_audience_movie\" itemprop=\"typicalAgeRange\">([0-9+]+)</div>";
         String regexSeance = "\s<div class=\"rasp_type\">([\sa-яА-яA-Za-z0-9]+)<i class=\"ruble\"></i></div>";
         String inputLine;
-        StringBuilder movie = new StringBuilder();
+        String movie = "";
         boolean checkSeance = false;
+        boolean checkTime = true;
         while ((inputLine = in.readLine()) != null) {
-            Matcher matcherMovie = getMatcher(regexMovie, inputLine);
-            Matcher matcherGenre = getMatcher(regexGenre, inputLine);
-            Matcher matcherAge = getMatcher(regexAge, inputLine);
-            Matcher matcherSeance = getMatcher(regexSeance, inputLine);
-            Matcher matcherTime = getMatcher("\s([0-9]{2}:[0-9]{2})", inputLine);
+            Matcher matcherMovie = getMatcher(regexMovie,inputLine);
+            Matcher matcherGenre = getMatcher(regexGenre,inputLine);
+            Matcher matcherAge = getMatcher(regexAge,inputLine);
+            Matcher matcherSeance = getMatcher(regexSeance,inputLine);
+            Matcher matcherTime = getMatcher("\s([0-9]{2}:[0-9]{2})",inputLine);
             if (matcherMovie.find()) {
+                checkTime = false;
                 if(checkSeance) {
-                    if (!result.toString().equals(""))
-                        result.append("\n");
-                    result.append(movie);
+                    if (!result.equals(""))
+                    {
+                        result+="\n\n";
+                    }
+                    result += movie;
                     checkSeance = false;
                 }
-                movie = new StringBuilder(matcherMovie.group(1) + "\n");
+                movie = matcherMovie.group(1)+"\n";
                 continue;
             }
             if (matcherGenre.find()) {
-                movie.append(matcherGenre.group(1)).append("\n");
+                movie += matcherGenre.group(1) + "\n";
                 continue;
             }
             if (matcherAge.find()) {
-                movie.append(matcherAge.group(1)).append("\n");
+                movie+=matcherAge.group(1)+"\n";
                 continue;
             }
             if (matcherSeance.find()) {
-                movie.append(matcherSeance.group(1)).append("\n");
+                if(checkTime)
+                {
+                    movie+="\n";
+                    checkTime = false;
+                }
+                movie+=matcherSeance.group(1)+"\n";
                 checkSeance = true;
                 continue;
             }
-            if (matcherTime.find())
-                movie.append(matcherTime.group(1)).append("\n");
+            if (matcherTime.find()) {
+                if(checkTime)
+                {
+                    movie+=", ";
+                }
+                movie+=matcherTime.group(1);
+                checkTime = true;
+                continue;
+            }
         }
         if(checkSeance) {
-            if (!result.toString().equals(""))
-                result.append("\n");
-            result.append(movie);
+            if (!result.equals(""))
+            {
+                result+="\n";
+            }
+            result += movie;
+            checkSeance = false;
         }
-        if(result.toString().equals(""))
+        if(result.equals(""))
+        {
             return "В этом кинотеатре сегодня нет сеансов. Попробуйте другой";
-        return result.toString();
+        }
+        return result;
     }
 }
